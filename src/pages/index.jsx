b1917ea0d9030dd9225158e3a112f9495b05c724
K-Kaji1226 <rashsea1127@gmail.com>
2023-04-09 11:39:36 +0900

    試しにクリックイベントを書いてみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,17 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+	const foo = 1;
+
+	// useCallbackを使うことによって、関数が再生成されなくなる
+	const handleClick = useCallback((e) => {
+		e.preventDefault();
+		console.log(foo);
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -12,6 +21,9 @@ export default function Home() {
 			</Head>
 
 			<Header />
+			<a href="/about" onClick={handleClick}>
+				ボタン
+			</a>
 			<Main page="index" />
 
 			<Footer />
