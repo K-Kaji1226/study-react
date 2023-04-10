e3a11fd04153b755950f9711a58db7792f61c5f2
K-Kaji1226 <rashsea1127@gmail.com>
2023-04-10 22:48:22 +0900

    useReducerの使い方を学びました。

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useCallback, useReducer } from "react";
+
+const initialState = {
+	data: [],
+	loading: true,
+	error: null,
+};
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "end":
+			return {
+				...state,
+				data: action.data,
+				loading: false,
+			};
+
+		case "error":
+			return {
+				...state,
+				loading: false,
+				error: action.error,
+			};
+		default:
+			throw new Error("no such action type!!!!");
+	}
+};
+
+export const Posts = () => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	// dataをフェッチ
+	const getPosts = useCallback(async () => {
+		try {
+			const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+			if (!res.ok) {
+				throw new Error("エラーが発生したため、データの取得に失敗しました。");
+			}
+			const json = await res.json();
+			// dispatchに入れるのはaction
+			dispatch({ type: "end", data: json });
+		} catch (error) {
+			dispatch({ type: "error", error });
+		}
+	}, []);
+
+	useEffect(() => {
+		getPosts();
+	}, [getPosts]);
+
+	if (state.loading) {
+		return <div>Loading...</div>;
+	}
+	if (state.error) {
+		return <div>{state.error.message}</div>;
+	}
+	if (state.data.length === 0) {
+		return <div>データは空です</div>;
+	}
+	return (
+		<ol>
+			{state.data.map((post) => (
+				<li key={post.id}>{post.title}</li>
+			))}
+		</ol>
+	);
+};
