6151712a20f7b04e26c2972e8c4554854489c568
K-Kaji1226 <rashsea1127@gmail.com>
2023-04-12 23:25:35 +0900

    ✨ ユーザーのfetch、fetcherの共通化

--- src/hooks/usePosts.jsx
+++ src/hooks/usePosts.jsx
@@ -1,13 +1,5 @@
+import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
-
-const fetcher = async (url) => {
-	const res = await fetch(url);
-	if (!res.ok) {
-		throw new Error("エラーが発生したため、データが取得できません。");
-	}
-	const json = await res.json();
-	return json;
-};
 
 export const usePosts = () => {
 	const { data, isLoading, error } = useSWR(
